5927bbfa512b4206094bd00709fc883756d7ac42
Lu Ho <l.hoatarek7777@senzma.cz>
2020-04-28 19:27:27 +0200

    free cx code

--- GlobalConfig empty.js
+++ GlobalConfig empty.js
@@ -2,8 +2,13 @@
 
 
 with(GlobalConfig){
-	cx = "";
+	
+	//https://cse.google.cz/
+	cx = "000888210889775888983:y9tkcjel090";// free code !!! NO Images https://developers.google.com/custom-search/docs/more_examples
+	
+	//https://developers.google.com/maps/documentation/javascript/get-api-key#restrict_key
 	APIKey = "";
+	
 }
 
 
